2ff0b8deeff0aec5e8282bee43d7fd5c1319e0c2
Kelly Carvalho <kelly.carvalho@alura.com.br>
2023-05-26 15:20:10 -0300

    init project

---
+++ src/test/java/br/com/fiap/techchallenge/TechChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
