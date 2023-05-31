91abdba919592057c9375b6cd7638763a34c25d1
Kelly Carvalho <kelly.carvalho@alura.com.br>
2023-05-31 12:00:45 -0300

    validação de update getById

--- src/main/java/br/com/fiap/techchallenge/address/AddressController.java
+++ src/main/java/br/com/fiap/techchallenge/address/AddressController.java
@@ -1,25 +1,25 @@
 package br.com.fiap.techchallenge.address;
 
-import jakarta.validation.*;
+import br.com.fiap.techchallenge.exception.NotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
 
 @RestController
 @RequestMapping("/address")
 public class AddressController {
 
     private final AddressRepository addressRepository;
+    private final AddressService addressService;
 
-    public AddressController(AddressRepository addressRepository) {
+    public AddressController(AddressRepository addressRepository, AddressService addressService) {
         this.addressRepository = addressRepository;
+        this.addressService = addressService;
     }
 
     @GetMapping
@@ -29,11 +29,32 @@ public class AddressController {
         return ResponseEntity.ok().body(addressDTOS);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> getAddress(@PathVariable Long id){
+        try{
+            AddressDTO address = addressService.findById(id);
+            return ResponseEntity.ok().body(address);
+        }catch (NotFoundException e){
+            String errorMessage = "Endereço não encontrado para o ID: "+id;
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);        }
+    }
+
     @PostMapping
     public ResponseEntity<?> createAddress(@Valid @RequestBody AddressDTO addressDTO){
         Address address = addressRepository.save(addressDTO.toEntity());
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(address.getId()).toUri();
         return ResponseEntity.created(uri).body(address);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody AddressDTO addressDTO){
+        try {
+            addressDTO = addressService.update(id, addressDTO);
+            return ResponseEntity.ok().body(addressDTO);
+        }catch (NotFoundException e){
+            String errorMessage = "Endereço não encontrado para o ID: "+id;
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+        }
     }
 }
--- src/main/java/br/com/fiap/techchallenge/address/AddressService.java
+++ src/main/java/br/com/fiap/techchallenge/address/AddressService.java
@@ -1,4 +1,31 @@
 package br.com.fiap.techchallenge.address;
 
+import br.com.fiap.techchallenge.exception.NotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AddressService {
+
+    private final AddressRepository addressRepository;
+
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    public AddressDTO update(Long id, AddressDTO addressDTO){
+        Address address = addressRepository.findById(id).orElseThrow(() -> new NotFoundException("Endereço não encontrado"));
+        address.setCity(addressDTO.city());
+        address.setNeighborhood(addressDTO.neighborhood());
+        address.setState(addressDTO.state());
+        address.setStreet(addressDTO.street());
+        address.setNumber(addressDTO.number());
+        addressRepository.save(address);
+        return new AddressDTO(address);
+    }
+
+    public AddressDTO findById(Long id){
+        Address address = addressRepository.findById(id).orElseThrow(() -> new NotFoundException("Endereço não encontrado"));
+        return new AddressDTO(address);
+    }
+
 }
