262ff1eccc3823832076342c629ce4e55339bd27
Sergio Bezerra da Silva <50722925+sergiobsilva2505@users.noreply.github.com>
2023-06-09 18:10:51 -0300

    retirando collections

--- src/main/java/br/com/fiap/techchallenge/appliance/ApplianceCollectionRepository.java
+++ src/main/java/br/com/fiap/techchallenge/appliance/ApplianceCollectionRepository.java
@@ -7,29 +7,7 @@ import java.util.*;
 @Repository
 public class ApplianceCollectionRepository  {
 
-    static private final Set<Appliance> appliances;
-
-    static {
-        appliances = new LinkedHashSet<>();
-
-        Appliance fridge = new Appliance("Refrigerador smart", "LG", "UVNANO 611L", 200, Voltage.ONE_HUNDRED_TWENTY_SEVEN);
-        Appliance stove = new Appliance("Fogão 5 bocas", "Atlas", "B097NC4JWK", null, Voltage.BIVOLT);
-        Appliance microwave = new Appliance("Forno de microondas", "Midea", "MRAS22", 700, Voltage.ONE_HUNDRED_TWENTY_SEVEN);
-        Appliance electricOven = new Appliance("Forno Elétrico", "Fischer", "26817-58801", 1750, Voltage.TWO_HUNDRED_AND_TWENTY);
-        Appliance washingMachine = new Appliance("Lavadora de Roupas 11kg", "Midea", "", null, Voltage.ONE_HUNDRED_TWENTY_SEVEN);
-        Appliance dishWasher = new Appliance("Lava louças", "Britânia", "BLL10B", null, Voltage.ONE_HUNDRED_TWENTY_SEVEN);
-        Appliance washingTank = new Appliance("Tanquinho Semiautomático", "Colormaq", "LCS10", null, Voltage.TWO_HUNDRED_AND_TWENTY);
-        Appliance airPurifier = new Appliance("Depurador de ar", "Suggar", "DPS181PR", null, Voltage.ONE_HUNDRED_TWENTY_SEVEN);
-
-        appliances.add(fridge);
-        appliances.add(stove);
-        appliances.add(microwave);
-        appliances.add(electricOven);
-        appliances.add(washingMachine);
-        appliances.add(dishWasher);
-        appliances.add(washingTank);
-        appliances.add(airPurifier);
-    }
+    static private final Set<Appliance> appliances  = new LinkedHashSet<>();
 
     public Collection<Appliance> findAll() {
         return appliances;
