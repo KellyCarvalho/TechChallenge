7d3c9835be7182a5a908eeff17881eef6814c678
Sergio Bezerra da Silva <50722925+sergiobsilva2505@users.noreply.github.com>
2023-06-09 17:58:56 -0300

    fix(main): not found exception message

--- src/main/java/br/com/fiap/techchallenge/exception/NotFoundException.java
+++ src/main/java/br/com/fiap/techchallenge/exception/NotFoundException.java
@@ -1,4 +1,8 @@
 package br.com.fiap.techchallenge.exception;
 
 public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
