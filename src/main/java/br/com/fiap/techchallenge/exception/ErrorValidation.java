57452ba35efc074e4c2d2934795a42784f1bfe76
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2023-08-26 12:10:39 -0300

    Adicionando tratamento de erro HttpMessageNotReadableException

--- src/main/java/br/com/fiap/techchallenge/exception/ErrorValidation.java
+++ src/main/java/br/com/fiap/techchallenge/exception/ErrorValidation.java
@@ -1,8 +1,7 @@
 package br.com.fiap.techchallenge.exception;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +14,7 @@ public class ErrorValidation {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
@@ -24,6 +22,12 @@ public class ErrorValidation {
             errors.put(fieldName, errorMessage);
         });
         return errors;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public Map<String, String> handleMessageNotReadableExceptions(HttpMessageNotReadableException ex) {
+        return Map.of("error", ex.getMessage());
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
