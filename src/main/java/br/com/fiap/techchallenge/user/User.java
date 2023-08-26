516d6e3640263446b79c43cfde8cd941d1a81bca
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2023-08-26 12:09:44 -0300

    Criando entidade User

---
+++ src/main/java/br/com/fiap/techchallenge/user/User.java
@@ -0,0 +1,31 @@
+package br.com.fiap.techchallenge.user;
+
+import br.com.fiap.techchallenge.person.*;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.*;
+
+@Entity
+@DiscriminatorValue("USER")
+public class User extends Person {
+
+    @OneToMany(mappedBy = "user")
+    private Collection<RelatedPerson> relatedPersons = new HashSet<>();
+
+    @Deprecated
+    public User() {
+    }
+
+    public User(String name, LocalDate birthDate, Gender gender) {
+        super(name, birthDate, gender);
+    }
+
+    public Collection<RelatedPerson> getRelatedPersons() {
+        return Collections.unmodifiableCollection(relatedPersons);
+    }
+
+    public void addRelatedPersons(RelatedPerson relatedPersons) {
+        this.relatedPersons.add(relatedPersons);
+    }
+}
---
+++ src/main/java/br/com/fiap/techchallenge/user/UserRepository.java
@@ -0,0 +1,6 @@
+package br.com.fiap.techchallenge.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
