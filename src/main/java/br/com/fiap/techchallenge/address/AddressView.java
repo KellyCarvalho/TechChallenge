761269a27121a042ed84cdfae3c2fd904685b7a3
Sergio Bezerra da Silva <sergio.bezerra@outlook.com>
2023-08-31 17:23:40 -0300

    user and person view in addressView

--- src/main/java/br/com/fiap/techchallenge/address/AddressView.java
+++ src/main/java/br/com/fiap/techchallenge/address/AddressView.java
@@ -1,9 +1,14 @@
 package br.com.fiap.techchallenge.address;
 
 
-public record AddressView(Long id, String street, String number, String neighborhood, String city, String state, String cep) {
+import br.com.fiap.techchallenge.person.PersonView;
+import br.com.fiap.techchallenge.user.UserView;
+
+import java.util.Collection;
+
+public record AddressView(Long id, String street, String number, String neighborhood, String city, String state, String cep, UserView userView, Collection<PersonView> peopleView) {
 
     public AddressView(Address address) {
-        this(address.getId(), address.getStreet(), address.getNumber(), address.getNeighborhood(), address.getCity(), address.getState(), address.getCep());
+        this(address.getId(), address.getStreet(), address.getNumber(), address.getNeighborhood(), address.getCity(), address.getState(), address.getCep(), new UserView(address.getUser()), PersonView.toPersonViewList(address.getPeople()));
     }
 }
--- src/main/java/br/com/fiap/techchallenge/person/PersonView.java
+++ src/main/java/br/com/fiap/techchallenge/person/PersonView.java
@@ -3,9 +3,14 @@ package br.com.fiap.techchallenge.person;
 import br.com.fiap.techchallenge.user.UserView;
 
 import java.time.LocalDate;
+import java.util.Collection;
 
 public record PersonView(Long id, String name, LocalDate birthDate, Gender gender, Connection connectionToUser, UserView relatedUser) {
     public PersonView(Person person) {
         this(person.getId(), person.getName(), person.getBirthDate(), person.getGender(), person.getConnectionToUser(), new UserView(person.getUser()));
+    }
+
+    public static Collection<PersonView> toPersonViewList(Collection<Person> people) {
+        return people.stream().map(PersonView::new).toList();
     }
 }
